556ee4ab05e4d53f7f058a7c001aa3a706337686
zaki ahmed <zakiahmed@zakis-MacBook-Pro.local>
2022-12-15 09:02:11 -0600

    new changes

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const searchButonEl = document.getElementById('search-button');
+const usersInputEl = document.getElementById('city-input');
+
+searchButonEl.addEventListener('click',function(){
+    console.log(usersInputEl.value);
+    // usersInputEl.value
+});
+
+
