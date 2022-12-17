24b1dac974fa3d99a22197fa6538e310dda58916
zaki ahmed <zakiahmed@zakis-MacBook-Pro.local>
2022-12-17 02:09:08 -0600

    new commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,10 @@
+//DOM emelements,API work
 const searchButonEl = document.getElementById('search-button');
 const usersInputEl = document.getElementById('city-input');
+const apiKey = '2b80e0fea04b57f6410d51502a9c7c22';
+const city = usersInputEl.value;
 
 searchButonEl.addEventListener('click',function(){
+    
     console.log(usersInputEl.value);
-    // usersInputEl.value
 });
-
-
