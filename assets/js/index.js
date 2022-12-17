47148560e304919040a726d20a852943289464dc
zaki ahmed <zakiahmed@zakis-MacBook-Pro.local>
2022-12-17 05:24:21 -0600

    new add

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,27 @@
-//DOM emelements,API work
+//DOM emelements,API 
+const apiKey = '2b80e0fea04b57f6410d51502a9c7c22';
 const searchButonEl = document.getElementById('search-button');
 const usersInputEl = document.getElementById('city-input');
-const apiKey = '2b80e0fea04b57f6410d51502a9c7c22';
-const city = usersInputEl.value;
+const curentWeather = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={2b80e0fea04b57f6410d51502a9c7c22}'
 
+
+//fetching api 
 searchButonEl.addEventListener('click',function(){
+    const city = usersInputEl.value;
     
-    console.log(usersInputEl.value);
-});
+    console.log(city)
+    });
+function fetchCurrent (){
+        fetch(curentWeather,{
+            method: 'GET',
+            credentials: 'same-origin',
+            redirect: 'follow',
+        })
+        .then(function (response){
+          response.json();  
+        })
+        .then(function(data){
+            console.log(data);
+        })
+}
+console.log(fetchCurrent)
