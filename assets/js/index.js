b47de8522d3cbfc711eca3ca7aeb5ca11585e208
zaki ahmed <zakiahmed@zakis-MacBook-Pro.local>
2022-12-18 02:38:16 -0600

    new commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,19 @@
 //DOM emelements,API 
-const apiKey = '2b80e0fea04b57f6410d51502a9c7c22';
+const APIKey = '2b80e0fea04b57f6410d51502a9c7c22';
 const searchButonEl = document.getElementById('search-button');
 const usersInputEl = document.getElementById('city-input');
-const curentWeather = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={2b80e0fea04b57f6410d51502a9c7c22}'
 
 
 //fetching api 
 searchButonEl.addEventListener('click',function(){
-    const city = usersInputEl.value;
-    
-    console.log(city)
-    });
-function fetchCurrent (){
-        fetch(curentWeather,{
-            method: 'GET',
-            credentials: 'same-origin',
-            redirect: 'follow',
-        })
-        .then(function (response){
-          response.json();  
-        })
-        .then(function(data){
-            console.log(data);
-        })
-}
-console.log(fetchCurrent)
+  const city = usersInputEl.value;
+  const curentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&Appid' + APIKey + '&units=imperial';
+  
+      fetch(curentWeather)
+      .then(function (response){
+        response.json();  
+      })
+      .then(function (data){
+        
+      })
+})
