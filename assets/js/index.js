342a4b44599280ffea6359aeaa04ac74878a7c39
zaki ahmed <zakiahmed@zakis-MacBook-Pro.local>
2022-12-20 04:59:25 -0600

    new adds

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,37 @@
-//DOM emelements,API 
-const APIKey = '78413dd4b50d8c825aabcc715b58e98b';
-const searchButonEl = document.getElementById('search-button');
-const usersInputEl = document.getElementById('city-input');
-const displayCurent = document.getElementById('current-day');
+//DOM emelements,API
+const APIKey = "78413dd4b50d8c825aabcc715b58e98b";
+const searchButonEl = document.getElementById("search-button");
+const usersInputEl = document.getElementById("city-input");
+const displayCurent = document.getElementById("current-day");
+const main = document.getElementById("main-temp");
+const weather = document.getElementById("temp");
+const wind = document.getElementById("wind-speed");
+//fetching api
 
-//fetching api 
+searchButonEl.addEventListener("click", function () {
+  const city = usersInputEl.value;
+  const curentWeather =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
 
-  searchButonEl.addEventListener('click',function(){
-    const city = usersInputEl.value;
-    const curentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey + '&units=imperial';
-    
-        fetch(curentWeather)
-        .then(function (response){
-          console.log(response.status)
-          if (response.status !== 200){
-            displayCurent.textContent = response.status
-          }
-          return response.json();  
-        })
-        .then(function (data){
-          console.log(data.response)
-          for (let i = 0; i < data.response; i++){
-            displayCurent.innerHTML = data.response;
-          }
-        })
-  })
+  fetch(curentWeather)
+    .then(function (response) {
+      console.log(response.status);
+      if (response.status !== 200) {
+        displayCurent.textContent = response.status;
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      main.textContent = data.weather[0].description;
+      wind.textContent = data.wind.speed;
+      main.textContent = data.main.feels_like;
+    });
+});
+// })
 
-function getData(){
-
-}
+function getData() {}
